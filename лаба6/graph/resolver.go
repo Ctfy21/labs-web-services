0fdb6f5ca99fd5d3fca76665495a44920c021a1a
Ctfy21 <skileev@bk.ru>
2024-10-03 16:37:15 +0300

    first commit

---
+++ лаба6/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/Ctfy21/lab6-web-services/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	commentaries []*model.Commentary
+}
---
+++ лаба6/repository/postgres_repository.go
@@ -0,0 +1,90 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/Ctfy21/lab6-web-services/graph/model"
+	_ "github.com/lib/pq"
+)
+
+type CommentaryRepository interface {
+	AddLike(commentarty_id int) error
+	RemoveLike(commentarty_id int) error
+	FindAll() []*model.Commentary
+	FindOne(commentarty_id int) *model.Commentary
+}
+
+type database struct {
+	client *sql.DB
+}
+
+func New() CommentaryRepository {
+
+	connStr := "postgres://postgres:123456@localhost/lab4_design_web_services?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to PostgreSQL")
+
+	return &database{
+		client: db,
+	}
+}
+
+func (db *database) AddLike(commentarty_id int) error {
+	_, err := db.client.Exec("UPDATE commentary SET count_likes = count_likes + 1 WHERE $1", commentarty_id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *database) RemoveLike(commentarty_id int) error {
+	_, err := db.client.Exec("UPDATE commentary SET count_likes = count_likes - 1 WHERE $1", commentarty_id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *database) FindAll() []*model.Commentary {
+	rows, err := db.client.Query("select * from commentary")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	commentaries := []*model.Commentary{}
+
+	for rows.Next() {
+		com := model.Commentary{}
+		err := rows.Scan(&com.CommentartyID, &com.UserID, &com.Topic, &com.Content, &com.CountLikes, &com.ThemeID, &com.Timestamp, &com.MessageType, &com.ReplyMessageID, &com.TagID)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		commentaries = append(commentaries, &com)
+	}
+	return commentaries
+}
+
+func (db *database) FindOne(commentarty_id int) *model.Commentary {
+	rows, err := db.client.Query("SELECT * FROM commentary WHERE commentary_id = $1", commentarty_id)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	com := model.Commentary{}
+	for rows.Next() {
+		err := rows.Scan(&com.CommentartyID, &com.UserID, &com.Topic, &com.Content, &com.CountLikes, &com.ThemeID, &com.Timestamp, &com.MessageType, &com.ReplyMessageID, &com.TagID)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
+	return &com
+}
